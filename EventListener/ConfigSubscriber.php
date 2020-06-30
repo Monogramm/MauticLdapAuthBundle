f2d778ad24bd89a75b3c508ced40e5c1ae96ce6e
Girish Ramakrishnan <girish@cloudron.io>
2020-06-30 16:33:34 -0700

    CommonSubscriber is removed in Mautic 3
    See https://github.com/mautic/mautic/pull/8148

--- EventListener/ConfigSubscriber.php
+++ EventListener/ConfigSubscriber.php
@@ -12,12 +12,12 @@ namespace MauticPlugin\MauticLdapAuthBundle\EventListener;
 use Mautic\ConfigBundle\ConfigEvents;
 use Mautic\ConfigBundle\Event\ConfigBuilderEvent;
 use Mautic\ConfigBundle\Event\ConfigEvent;
-use Mautic\CoreBundle\EventListener\CommonSubscriber;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 
 /**
  * Class ConfigSubscriber.
  */
-class ConfigSubscriber extends CommonSubscriber
+class ConfigSubscriber implements EventSubscriberInterface
 {
     /**
      * @return array
